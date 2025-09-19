137a824e2cd66d87374f9196fb3b73015ae82c08
Deiviz <davidskisnot@gmail.com>
2025-09-19 21:08:26 +0200

    Actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { EquipoDirectivoComponent } from "./components/equipo-directivo/equipo-directivo.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent,RouterModule],
+  imports: [RouterOutlet, NavBarComponent, RouterModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
